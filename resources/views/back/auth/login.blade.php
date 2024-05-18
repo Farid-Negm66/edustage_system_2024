e37ccde4af4d8abf03ea9d3bb2036e4c129b8669
unknown <faridnegm44@gmail.com>
2024-05-18 16:52:58 +0300

    login script p1

--- resources/views/back/auth/login.blade.php
+++ resources/views/back/auth/login.blade.php
@@ -1,11 +1,7 @@
 
-
-
 <!DOCTYPE html>
-<html lang="en">
-	
-<!-- Mirrored from codeigniter.spruko.com/valex/rtl/public/pages/signin by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 05 Jan 2022 09:50:17 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<html lang="en" dir="rtl">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
 
 		<meta charset="UTF-8">
@@ -15,34 +11,50 @@
 		<meta name="Author" content="Spruko Technologies Private Limited">
 		<meta name="Keywords" content="dashboard, admin, bootstrap admin template, codeigniter, php, php framework, codeigniter 4, php mvc, php codeigniter, best php framework, codeigniter admin, codeigniter dashboard, admin panel template, bootstrap 4 admin template, bootstrap dashboard template"/>
 
-        		<!-- Title -->
+		<!-- Title -->
 		<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
 
 		<!-- Favicon -->
-		<link rel="icon" href="{{ url('back') }}/assets/img/brand/favicon.png" type="image/x-icon"/>
+		<link rel="icon" href="{{ asset('back') }}/assets/img/brand/favicon.png" type="image/x-icon"/>
 
 		<!-- Bootstrap css-->
-		<link href="{{ url('back') }}/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+		<link href="{{ asset('back') }}/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
 
 		<!-- Icons css -->
-		<link href="{{ url('back') }}/assets/css-rtl/icons.css" rel="stylesheet">
+		<link href="{{ asset('back') }}/assets/css-rtl/icons.css" rel="stylesheet">
 
         <!-- P-scroll bar css remove in final -->
-		<link href="{{ url('back') }}/assets/plugins/perfect-scrollbar/p-scrollbar.css" rel="stylesheet" />
+		<link href="{{ asset('back') }}/assets/plugins/perfect-scrollbar/p-scrollbar.css" rel="stylesheet" />
 
 		<!-- Style css -->
-		<link href="{{ url('back') }}/assets/css-rtl/style.css" rel="stylesheet">
+		<link href="{{ asset('back') }}/assets/css-rtl/style.css" rel="stylesheet">
 
 		<!-- Dark-mode css -->
-		<link href="{{ url('back') }}/assets/css-rtl/style-dark.css" rel="stylesheet">
+		<link href="{{ asset('back') }}/assets/css-rtl/style-dark.css" rel="stylesheet">
 
         
 		<!---Skinmodes css-->
-		<link href="{{ url('back') }}/assets/css-rtl/skin-modes.css" rel="stylesheet" />
+		<link href="{{ asset('back') }}/assets/css-rtl/skin-modes.css" rel="stylesheet" />
 
 		<!---Switcher css-->
-		<link href="{{ url('back') }}/assets/switcher/css/switcher-rtl.css" rel="stylesheet">
-		<link href="{{ url('back') }}/assets/switcher/demo.css" rel="stylesheet">
+		<link href="{{ asset('back') }}/assets/switcher/css/switcher-rtl.css" rel="stylesheet">
+		<link href="{{ asset('back') }}/assets/switcher/demo.css" rel="stylesheet">
+
+		<link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=Cairo:slnt,wght@11,200..1000&family=Changa:wght@200..800&display=swap" rel="stylesheet">
+
+		<style>
+			@font-face {
+				font-family: "4_F4";
+				src: url("{{ asset('back/fonts/4_F4.ttf') }}");
+			}
+			body{
+				/* font-family: Arial, Helvetica, sans-serif, serif; */
+				/* font-family: "4_F4", serif; */
+				font-family: Almarai;
+				
+			}
+
+		</style>
     </head>
 	<body class="main-body">
 			
@@ -63,17 +75,17 @@
 							<div class="swichermainleft">
 								<h4>Navigation Style</h4>
 								<div class="pl-3 pr-3">
-									<a href="{{ url('back') }}/../pages/horizontal-light.html" class="btn btn-danger btn-block mt-0">Horizontal</a>
-									<a href="{{ url('back') }}/../pages/icon-light.html" class="btn btn-info btn-block">Left-menu</a>
+									<a href="{{ asset('back') }}/../pages/horizontal-light.html" class="btn btn-danger btn-block mt-0">Horizontal</a>
+									<a href="{{ asset('back') }}/../pages/icon-light.html" class="btn btn-info btn-block">Left-menu</a>
 								</div>
 							</div>
 							<div class="swichermainleft">
 								<h4>Skin Modes</h4>
 								<div class="pl-0 pr-0">
-									<a class="wscolorcode blackborder nav-hor navstyle1" href="{{ url('back') }}/../pages/icon-light.html">
+									<a class="wscolorcode blackborder nav-hor navstyle1" href="{{ asset('back') }}/../pages/icon-light.html">
 										Light-theme
 									</a>
-									<a class="wscolorcode blackborder nav-hor navstyle1" href="{{ url('back') }}/../pages/icon-dark.html">
+									<a class="wscolorcode blackborder nav-hor navstyle1" href="{{ asset('back') }}/../pages/icon-dark.html">
 										Dark-theme
 									</a>
 								</div>
@@ -148,7 +160,7 @@
 		<!-- End Switcher -->
 		<!-- Loader -->
 		<div id="global-loader">
-			<img src="{{ url('back') }}/assets/img/loader.svg" class="loader-img" alt="Loader">
+			<img src="{{ asset('back') }}/assets/img/loader.svg" class="loader-img" alt="Loader">
 		</div>
 		<!-- /Loader -->
 
@@ -158,47 +170,35 @@
 			<div class="page">
 
 				<div class="container-fluid">
-					<div class="row no-gutter">
-						<!-- The image half -->
-						<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
-							<div class="row wd-100p mx-auto text-center">
-								<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-									<img src="{{ url('back') }}/assets/img/media/login.png" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
-								</div>
-							</div>
-						</div>
-						<!-- The content half -->
-						<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+					<div class="row no-gutter">						
+						
+						<div class="col-md-6 col-lg-6 col-xl-5 bg-gray">
 							<div class="login d-flex align-items-center py-2">
 								<!-- Demo content-->
 								<div class="container p-0">
 									<div class="row">
 										<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
 											<div class="card-sigin">
-												<div class="mb-5 d-flex"> <a href="index.html"><img src="{{ url('back') }}/assets/img/brand/favicon.png" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 mr-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
+												<div class="mb-5 d-flex">
+													<a href="index.html"><img src="{{ asset('back/images/settings/logo.png') }}" class="sign-favicon ht-60" alt="logo"></a>
+													<h1 class="main-logo1 mr-1 mr-0 my-auto tx-22" style="padding-top: 14px;">أكاديمية اديوستديج</h1>
+												</div>
+
 												<div class="card-sigin">
 													<div class="main-signup-header">
-														<h2>Welcome back!</h2>
-														<h5 class="font-weight-semibold mb-4">Please sign in to continue.</h5>
+														<h2 style="font-size: 23px;">مرحباً بك مرة أخرى!</h2>
+														<h5 class="font-weight-semibold mb-4" style="font-size: 17px;">يرجى تسجيل الدخول للمتابعة.</h5>
 														<form action="#">
 															<div class="form-group">
-																<label>Email</label> <input class="form-control" placeholder="Enter your email" type="text">
+																<label>البريد الإلكتروني</label> <input class="form-control" placeholder="البريد الإلكتروني" type="text">
 															</div>
 															<div class="form-group">
-																<label>Password</label> <input class="form-control" placeholder="Enter your password" type="password">
-															</div><button class="btn btn-main-primary btn-block">Sign In</button>
-															<div class="row row-xs">
-																<div class="col-sm-6">
-																	<button class="btn btn-block"><i class="fab fa-facebook-f"></i> Signup with Facebook</button>
-																</div>
-																<div class="col-sm-6 mg-t-10 mg-sm-t-0">
-																	<button class="btn btn-info btn-block"><i class="fab fa-twitter"></i> Signup with Twitter</button>
-																</div>
-															</div>
+																<label>الرقم السري</label> <input class="form-control" placeholder="الرقم السري" type="password">
+															</div><button class="btn btn-main-primary btn-block">تسجيل الدخول</button>
+
 														</form>
 														<div class="main-signin-footer mt-5">
-															<p><a href="#">Forgot password?</a></p>
-															<p>Don't have an account? <a href="signup.html">Create an Account</a></p>
+															<p><a href="#">نسيت كلمة المرور ؟</a></p>
 														</div>
 													</div>
 												</div>
@@ -207,7 +207,16 @@
 									</div>
 								</div><!-- End -->
 							</div>
-						</div><!-- End -->
+						</div>
+						
+						<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent" style="background: #fff !important;">
+							<div class="row wd-100p mx-auto text-center">
+								<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+									<img src="{{ asset('back/images/settings/login6.jpg') }}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+								</div>
+							</div>
+						</div>
+						
 					</div>
 				</div>
 
@@ -217,32 +226,32 @@
 
 	
         		<!-- JQuery min js -->
-		<script src="{{ url('back') }}/assets/plugins/jquery/jquery.min.js"></script>
+		<script src="{{ asset('back') }}/assets/plugins/jquery/jquery.min.js"></script>
 
 		<!-- Bootstrap js -->
-        <script src="{{ url('back') }}/assets/plugins/bootstrap/js/popper.min.js"></script>
-        <script src="{{ url('back') }}/assets/plugins/bootstrap/js/bootstrap-rtl.js"></script>
+        <script src="{{ asset('back') }}/assets/plugins/bootstrap/js/popper.min.js"></script>
+        <script src="{{ asset('back') }}/assets/plugins/bootstrap/js/bootstrap-rtl.js"></script>
 
 		<!-- Ionicons js -->
-		<script src="{{ url('back') }}/assets/plugins/ionicons/ionicons.js"></script>
+		<script src="{{ asset('back') }}/assets/plugins/ionicons/ionicons.js"></script>
 
 		<!-- P-scroll js Remove in final -->
-		<script src="{{ url('back') }}/assets/plugins/perfect-scrollbar/perfect-scrollbar.min-rtl.js"></script>
-		<script src="{{ url('back') }}/assets/plugins/perfect-scrollbar/p-scroll-rtl.js"></script>
+		<script src="{{ asset('back') }}/assets/plugins/perfect-scrollbar/perfect-scrollbar.min-rtl.js"></script>
+		<script src="{{ asset('back') }}/assets/plugins/perfect-scrollbar/p-scroll-rtl.js"></script>
 
 		<!-- eva-icons js -->
-		<script src="{{ url('back') }}/assets/js/eva-icons.min.js"></script>
+		<script src="{{ asset('back') }}/assets/js/eva-icons.min.js"></script>
 
 		<!-- Rating js-->
-		<script src="{{ url('back') }}/assets/plugins/rating/jquery.rating-stars.js"></script>
-		<script src="{{ url('back') }}/assets/plugins/rating/jquery.barrating.js"></script>
+		<script src="{{ asset('back') }}/assets/plugins/rating/jquery.rating-stars.js"></script>
+		<script src="{{ asset('back') }}/assets/plugins/rating/jquery.barrating.js"></script>
 
         
 		<!-- custom js -->
-		<script src="{{ url('back') }}/assets/js/custom.js"></script>
+		<script src="{{ asset('back') }}/assets/js/custom.js"></script>
 
 		<!-- Switcher js -->
-		<script src="{{ url('back') }}/assets/switcher/js/switcher-rtl.js"></script>
+		<script src="{{ asset('back') }}/assets/switcher/js/switcher-rtl.js"></script>
 
     </body>
 
--- resources/views/back/layouts/app.blade.php
+++ resources/views/back/layouts/app.blade.php
@@ -42,15 +42,15 @@
 
 
     <!--- Select2 css --->
-    <link href="{{ url('back') }}/assets/plugins/select2/css/select2.min.css" rel="stylesheet">
+    <link href="{{ asset('back') }}/assets/plugins/select2/css/select2.min.css" rel="stylesheet">
 
     <!-- Data table css -->
-    <link href="{{ url('back') }}/assets/plugins/datatable/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
-    <link href="{{ url('back') }}/assets/plugins/datatable/css/buttons.bootstrap4.min.css" rel="stylesheet">
-    <link href="{{ url('back') }}/assets/plugins/datatable/css/responsive.bootstrap4.min.css" rel="stylesheet" />
-    <link href="{{ url('back') }}/assets/plugins/datatable/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="{{ url('back') }}/assets/plugins/datatable/css/responsive.dataTables.min.css" rel="stylesheet">
-    <link href="{{ url('back') }}/assets/plugins/select2/css/select2.min.css" rel="stylesheet">
+    <link href="{{ asset('back') }}/assets/plugins/datatable/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <link href="{{ asset('back') }}/assets/plugins/datatable/css/buttons.bootstrap4.min.css" rel="stylesheet">
+    <link href="{{ asset('back') }}/assets/plugins/datatable/css/responsive.bootstrap4.min.css" rel="stylesheet" />
+    <link href="{{ asset('back') }}/assets/plugins/datatable/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="{{ asset('back') }}/assets/plugins/datatable/css/responsive.dataTables.min.css" rel="stylesheet">
+    <link href="{{ asset('back') }}/assets/plugins/select2/css/select2.min.css" rel="stylesheet">
 
     {{-- alertify --}}
     <link href="{{ asset('back/assets/css-rtl/alertify.rtl.min.css') }}" type="text/css" rel="stylesheet"/>
@@ -326,32 +326,32 @@
     {{-- <script src="{{ asset('back') }}/assets/js/jquery.vmap.sampledata.js"></script> --}}
     
     <!--Internal  Datepicker js -->
-    <script src="{{ url('back') }}/assets/plugins/jquery-ui/ui/widgets/datepicker.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/jquery-ui/ui/widgets/datepicker.js"></script>
 
     <!-- Internal Select2 js-->
-    <script src="{{ url('back') }}/assets/plugins/select2/js/select2.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/select2/js/select2.min.js"></script>
 
     <!-- Internal Modal js-->
-    <script src="{{ url('back') }}/assets/js/modal.js"></script>
+    <script src="{{ asset('back') }}/assets/js/modal.js"></script>
 
     <!-- Data tables -->
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/dataTables.dataTables.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/dataTables.responsive.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/responsive.dataTables.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/jquery.dataTables.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/dataTables.bootstrap4.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/dataTables.buttons.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/buttons.bootstrap4.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/jszip.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/pdfmake.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/vfs_fonts.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/buttons.html5.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/buttons.print.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/buttons.colVis.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/dataTables.responsive.min.js"></script>
-    <script src="{{ url('back') }}/assets/plugins/datatable/js/responsive.bootstrap4.min.js"></script>
-    <script src="{{ url('back') }}/assets/js/table-data.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/dataTables.dataTables.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/responsive.dataTables.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/jquery.dataTables.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/dataTables.bootstrap4.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/dataTables.buttons.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/buttons.bootstrap4.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/jszip.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/pdfmake.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/vfs_fonts.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/buttons.html5.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/buttons.print.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/buttons.colVis.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('back') }}/assets/plugins/datatable/js/responsive.bootstrap4.min.js"></script>
+    <script src="{{ asset('back') }}/assets/js/table-data.js"></script>
 
     <!-- alertify -->
     <script src="{{ asset('back/assets/js/alertify.min.js') }}"></script>
