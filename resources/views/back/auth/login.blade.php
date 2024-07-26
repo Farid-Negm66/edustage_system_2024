ae4e14877e91c35adadf3857f6aa962888bb2378
FaridNegm <faridnegm44@gmail.com>
2024-07-26 22:55:41 +0200

    new edits to login page design

--- resources/views/back/auth/login.blade.php
+++ resources/views/back/auth/login.blade.php
@@ -12,10 +12,10 @@
 		<meta name="Keywords" content="dashboard, admin, bootstrap admin template, codeigniter, php, php framework, codeigniter 4, php mvc, php codeigniter, best php framework, codeigniter admin, codeigniter dashboard, admin panel template, bootstrap 4 admin template, bootstrap dashboard template"/>
 
 		<!-- Title -->
-		<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+		<title> أكاديمية إديوستيدج | تسجيل الدخول </title>
 
 		<!-- Favicon -->
-		<link rel="icon" href="{{ asset('back') }}/assets/img/brand/favicon.png" type="image/x-icon"/>
+		<link rel="icon" href="{{ asset('back/images/settings/logo.png') }}" type="image/x-icon"/>
 
 		<!-- Bootstrap css-->
 		<link href="{{ asset('back') }}/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
@@ -58,106 +58,7 @@
     </head>
 	<body class="main-body">
 			
-                <!-- Start Switcher -->
-        <div class="switcher-wrapper ">
-			<div class="demo_changer">
-				<div class="demo-icon bg_dark"><i class="fa fa-cog fa-spin  text_primary"></i></div>
-				<div class="form_holder sidebar-right1">
-					<div class="row">
-						<div class="swichermainleft border-bottom  text-center">
-							<div class="p-3">
-								<a href="https://www.spruko.com/demo/valex/" class="btn btn-primary btn-block mt-0">View Demo</a>
-								<a href="https://themeforest.net/item/valex-bootstrap-admin-dashboard-html-template/26645744" class="btn btn-secondary btn-block">Buy Now</a>
-								<a href="https://themeforest.net/user/sprukosoft/portfolio" class="btn btn-success btn-block">Our Portfolio</a>
-							</div>
-						</div>
-						<div class="predefined_styles">
-							<div class="swichermainleft">
-								<h4>Navigation Style</h4>
-								<div class="pl-3 pr-3">
-									<a href="{{ asset('back') }}/../pages/horizontal-light.html" class="btn btn-danger btn-block mt-0">Horizontal</a>
-									<a href="{{ asset('back') }}/../pages/icon-light.html" class="btn btn-info btn-block">Left-menu</a>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Skin Modes</h4>
-								<div class="pl-0 pr-0">
-									<a class="wscolorcode blackborder nav-hor navstyle1" href="{{ asset('back') }}/../pages/icon-light.html">
-										Light-theme
-									</a>
-									<a class="wscolorcode blackborder nav-hor navstyle1" href="{{ asset('back') }}/../pages/icon-dark.html">
-										Dark-theme
-									</a>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Skin Modes</h4>
-								<div class="switch_section">
-									<div class="switch-toggle d-flex">
-										<span class="ml-auto">Default Body</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch" id="myonoffswitch7" class="onoffswitch2-checkbox" checked>
-											<label for="myonoffswitch7" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-									<div class="switch-toggle d-flex">
-										<span class="ml-auto">Body Style1</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch" id="myonoffswitch6" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch6" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Leftmenu Bg-Image</h4>
-								<div class="skin-body light-pattern">
-									<button type="button" id="leftmenuimage1" class="bg1 wscolorcode1 blackborder"></button>
-									<button type="button" id="leftmenuimage2" class="bg2 wscolorcode1 blackborder"></button>
-									<button type="button" id="leftmenuimage3" class="bg3 wscolorcode1 blackborder"></button>
-									<button type="button" id="leftmenuimage4" class="bg4 wscolorcode1 blackborder"></button>
-									<button type="button" id="leftmenuimage5" class="bg5 wscolorcode1 blackborder"></button>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Leftmenu Styles</h4>
-								<div class="switch_section">
-									<div class="switch-toggle horizontal-light-switcher d-flex">
-										<span class="ml-auto">Leftmenu Light</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch3" id="myonoffswitch9" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch9" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-									<div class="switch-toggle d-flex">
-										<span class="ml-auto">Leftmenu Color</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch3" id="myonoffswitch10" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch10" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-									<div class="switch-toggle horizontal-Dark-switcher d-flex">
-										<span class="ml-auto">Leftmenu Dark</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch3" id="myonoffswitch11" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch11" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-									<div class="switch-toggle d-flex">
-										<span class="ml-auto">Leftmenu Gradient Color</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch3" id="myonoffswitch12" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch12" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-									<div class="switch-toggle d-flex">
-										<span class="ml-auto">Reset Leftmenu Styles</span>
-										<div class="onoffswitch2"><input type="radio" name="onoffswitch3" id="myonoffswitch13" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch13" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- End Switcher -->
+               
 		<!-- Loader -->
 		<div id="global-loader">
 			<img src="{{ asset('back') }}/assets/img/loader.svg" class="loader-img" alt="Loader">
@@ -178,10 +79,10 @@
 								<div class="container p-0">
 									<div class="row">
 										<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
-											<div class="card-sigin">
-												<div class="mb-5 d-flex">
-													<a href="index.html"><img src="{{ asset('back/images/settings/logo.png') }}" class="sign-favicon ht-60" alt="logo"></a>
-													<h1 class="main-logo1 mr-1 mr-0 my-auto tx-22" style="padding-top: 14px;">أكاديمية اديوستديج</h1>
+											<div class="card-sigin" style="margin-top: -70px;">
+												<div class="mb-5 d-flex" style="margin-bottom: 2rem !important;">
+													<img src="{{ asset('back/images/settings/logo.png') }}" class="sign-favicon ht-60" alt="أكاديمية إديوستيدج">
+													<h1 class="main-logo1 mr-1 mr-0 my-auto" style="padding-top: 14px;font-size: 25px !important;text-decoration: underline;">أكاديمية إديوستيدج</h1>
 												</div>
 
 												<div class="card-sigin">
@@ -190,16 +91,38 @@
 														<h5 class="font-weight-semibold mb-4" style="font-size: 17px;">يرجى تسجيل الدخول للمتابعة.</h5>
 														<form action="#">
 															<div class="form-group">
-																<label>البريد الإلكتروني</label> <input class="form-control" placeholder="البريد الإلكتروني" type="text">
+																<label>البريد الإلكتروني</label> 
+																<input class="form-control" placeholder="البريد الإلكتروني" type="text">
 															</div>
+															
 															<div class="form-group">
-																<label>الرقم السري</label> <input class="form-control" placeholder="الرقم السري" type="password">
-															</div><button class="btn btn-main-primary btn-block">تسجيل الدخول</button>
+																<label>الرقم السري</label> 
+																<input class="form-control" placeholder="الرقم السري" type="password">
+															</div>
+															
+															<div class="form-group">
+																<label>حالة المستخدم</label> 
+																<select class="form-control" placeholder="حالة الشخص" name="" id="">
+																	<option value="1">سوبر أدمن</option>
+																	<option value="2">فريق العمل</option>
+																	<option value="3">مدرس</option>
+																	<option value="4">ولي أمر</option>
+																	<option value="5">طالب</option>
+																</select>
+															</div>
+															
+															<br>
+															<div class="form-group">
+																<button class="btn btn-lg btn-main-primary btn-block">
+																	تسجيل الدخول
+																	<i class="fas fa-angle-left" style="font-size: 22px;position: relative;top: 3px;right: 8px;color: #dfcb31;"></i>
+																</button>
+															</div>
 
 														</form>
-														<div class="main-signin-footer mt-5">
+														{{-- <div class="main-signin-footer mt-5">
 															<p><a href="#">نسيت كلمة المرور ؟</a></p>
-														</div>
+														</div> --}}
 													</div>
 												</div>
 											</div>
